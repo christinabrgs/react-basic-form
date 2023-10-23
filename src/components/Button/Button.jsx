c98148d620a25856c5a8c6590e2000a7fb7b9308
Christina <christina.brgs@gmail.com>
2023-10-23 15:39:32 -0400

    added a button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import '../Button/Button.css'
 
-export default function Button ({children}) {
-    return (
-        <button>
-            {children}
-        </button>
-    )
-}
+    const styles = {
+        button: {
+          padding: '10px 20px',
+          border: 'none',
+          borderRadius: '5px',
+          fontSize: '16px',
+          cursor: 'pointer',
+          outline: 'none',
+        },
+        primary: {
+          backgroundColor: '#007BFF',
+          color: '#ffffff',
+        },
+        secondary: {
+          backgroundColor: '#6C757D',
+          color: '#ffffff',
+        },
+      };
+
+    
+
+    export default function Button({ type = "button", children = "Default Label", variant="primary" }) {
+        return (
+            <button className="button" type={type} style={{ ...styles.button, ...styles[variant]}}>
+                {children}
+            </button>
+        );
+    }
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import '../Home/Home.css';
+import "../Home/Home.css";
 import Navbar from "../../Navbar/Navbar";
+import Button from "../../Button/Button";
 
-export default function Home () {
-    return (
-        <div>
-            <Navbar />
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className="home">
+      <Navbar />
+
+      <Button type="submit" children="Submit" variant='secondary'/>
+    </div>
+  );
 }
